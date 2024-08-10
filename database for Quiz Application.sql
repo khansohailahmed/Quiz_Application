7dc9c5dd4d79144f45156fcc1558ed69995d266a
Khan Sohail Tufail Ahmed <125254036+khansohailahmed@users.noreply.github.com>
2024-08-10 23:48:32 +0530

    Add files via upload
    This is a Database in MySQL format use in this project

---
+++ database for Quiz Application.sql
@@ -0,0 +1,6 @@
+create database	quizApp	;
+use quizApp;
+create	table login (name varchar(20),password varchar(20),phone varchar(20));
+select	* from login;
+create table score (name varchar(20),score varchar(20));
+select * from score;
